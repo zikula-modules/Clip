583c5271e426b056e8623bf456413f6f11445289
Marco Kundert <mk@sexyandfamous.com>
2008-06-20 18:07:41 +0000

    list.php block can be used instead with filter with pid=xxx

--- modules/pagemaster/pntemplates/plugins/function.pmformcheckboxinput.php
+++ modules/pagemaster/pntemplates/plugins/function.pmformcheckboxinput.php
@@ -1,11 +1,22 @@
 <?php
-require_once ('system/pnForm/plugins/function.pnformcheckbox.php');
+require_once ('system/pnForm/plugins/function.pnformcategorycheckboxlist.php');
 
-class pmformcheckboxinput extends pnFormCheckbox {
-	
+class pmformcheckboxinput extends pnFormCategoryCheckboxList {
+
 	var $columnDef = 'I(4)';
 	var $title = 'Checkbox';
-	
+	function load(& $render, $params)
+	{
+		$pubfields = $render->pnFormEventHandler->pubfields;
+		foreach ($pubfields as $key => $pubfield) {
+
+			if ($pubfield['name'] == $this->id) {
+				$params['category'] = $pubfield['typedata'];
+			}
+		}
+		parent :: load(& $render, $params);
+	}
+
 	function getFilename() {
 		return __FILE__; // FIXME: may be found in smarty's data???
 	}
