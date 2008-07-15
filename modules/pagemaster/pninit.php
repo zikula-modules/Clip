5f3b7bbda2a80b122d32c1fdbcdf585f36346d21
Mateo TibaPalacios <nestormateo@gmail.com>
2008-07-15 18:07:18 +0000

    Minor fixes in the pninit script

--- modules/pagemaster/pninit.php
+++ modules/pagemaster/pninit.php
@@ -11,13 +11,11 @@
 
 function pagemaster_init()
 {
-    Loader::loadClass('CategoryUtil');
-    Loader::loadClassFromModule('Categories', 'Category');
-
     // create table
     if (!DBUtil::createTable('pagemaster_pubfields')) {
         return false;
     }
+
     if (!DBUtil::createTable('pagemaster_pubtypes')) {
         return false;
     }
@@ -30,6 +28,9 @@ function pagemaster_init()
         return false;
     }
 
+    Loader::loadClass('CategoryUtil');
+    Loader::loadClassFromModule('Categories', 'Category');
+
     $rootcat = CategoryUtil::getCategoryByPath('/__SYSTEM__/Modules');
     $lang = pnUserGetLang();
 
@@ -41,22 +42,21 @@ function pagemaster_init()
     $cat->insert();
     $cat->update();
 
-    $rootcat = CategoryUtil :: getCategoryByPath('/__SYSTEM__/Modules/pagemaster');
+    $rootcat = CategoryUtil::getCategoryByPath('/__SYSTEM__/Modules/pagemaster');
     $cat = new PNCategory();
     $cat->setDataField('parent_id', $rootcat['id']);
     $cat->setDataField('name', 'lists');
-     $cat->setDataField('display_name', array ($lang => 'lists'));
+    $cat->setDataField('display_name', array ($lang => 'lists'));
     $cat->setDataField('display_desc', array ($lang => 'contains lists for pagemaster publications'));
     $cat->insert();
     $cat->update();
     return (true);
 }
 
-function pagemaster_upgrade($oldversion)
+function pagemaster_upgrade($from_version)
 {
-    $from_version = $oldversion;
-
     switch ($from_version) {
+
     case '0.1' :
         if (!DBUtil::changeTable('pagemaster_pubtypes')) {
             return LogUtil::registerError(_CHANGETABLEFAILED);
@@ -65,14 +65,17 @@ function pagemaster_upgrade($oldversion)
             return LogUtil::registerError(_CREATEINDEXFAILED);
         }
         $types = DBUtil::selectObjectArray('pagemaster_pubtypes');
-        foreach (array_keys($types) as $k) {
-            $types[$k]['urltitle'] = DataUtil::formatPermalink($types[$k]['title']);
-        }
-        if (!DBUtil::updateObjectArray($types, 'pagemaster_pubtypes')) {
-            return LogUtil::registerError(_UPDATEFAILED);
+        if ($types !== false) {
+            foreach (array_keys($types) as $k) {
+                $types[$k]['urltitle'] = DataUtil::formatPermalink($types[$k]['title']);
+            }
+            if (!DBUtil::updateObjectArray($types, 'pagemaster_pubtypes', 'tid')) {
+                return LogUtil::registerError(_UPDATEFAILED);
+            }
         }
     case '0.2':
         break;
+
     }
 
     return true;
@@ -80,8 +83,6 @@ function pagemaster_upgrade($oldversion)
 
 function pagemaster_delete()
 {
-    Loader::loadClass('CategoryUtil');
-
     $pubtypes = DBUtil::selectObjectArray('pagemaster_pubtypes');
     foreach ($pubtypes as $pubtype) {
         DBUtil::dropTable('pagemaster_pubdata'.$pubtype['tid']);
@@ -97,6 +98,7 @@ function pagemaster_delete()
         return false;
     }
 
+    Loader::loadClass('CategoryUtil');
     CategoryUtil::deleteCategoriesByPath('/__SYSTEM__/Modules/pagemaster', 'path');
     pnModDelVar('pagemaster', 'uploadpath');
 
