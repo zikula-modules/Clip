cd69b78f1811a05e4c2976ddc452bca3e70c20e4
Mateo TibaPalacios <nestormateo@gmail.com>
2008-07-16 22:39:20 +0000

    Create revision commented out until is fixed

--- modules/pagemaster/workflows/operations/function.createNewRevision.php
+++ modules/pagemaster/workflows/operations/function.createNewRevision.php
@@ -11,14 +11,17 @@
 
 function pagemaster_operation_createNewRevision(&$obj, $params)
 {
-    $online = isset($params['online']) ? $params['online'] : false;
-    $obj['core_online'] = $online;
-    if (!isset($params['nextstate']))
+/*
+    if (!isset($params['nextstate'])) {
         return;
+    }
+
+    $online = (isset($params['online']) && is_bool($params['online'])) ? $params['online'] : false;
+    $obj['core_online'] = $online;
 
     $nextState = $params['nextstate'];
-    
-    if ($online == 1){
+
+    if ($online == 1) {
         //set all other to offline
         $data = array('core_online' => 0);
         $result = DBUtil::updateObject($data, $obj['__WORKFLOW__']['obj_table'], 'pm_online = 1 and pm_pid = '.$obj['core_pid']);
@@ -30,6 +33,11 @@ function pagemaster_operation_createNewRevision(&$obj, $params)
     unset($new_rev['__WORKFLOW__']['id']);
     $workflow = new pnWorkflow($obj['__WORKFLOW__']['schemaname'],'pagemaster');
     $ret = $workflow->registerWorkflow($new_rev, $nextState);
-    $revision = array('tid' => $obj['tid'], 'id' => $new_rev['id'], 'pid' => $obj['core_pid'], 'prevversion' => 1 );
+    $revision = array('tid' => $obj['tid'],
+                      'id'  => $new_rev['id'],
+                      'pid' => $obj['core_pid'],
+                      'prevversion' => 1 );
     return DBUtil::insertObject($revision, 'pagemaster_revisions');
+*/
+    return true;
 }
