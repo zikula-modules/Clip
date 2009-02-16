ddf8bcf0dca7da1ad076650c5d0053e12fec52ae
Mateo TibaPalacios <nestormateo@gmail.com>
2009-02-16 21:06:07 +0000

    pmformuploadinput: removed dependency of upload path, needs 0.2.1 upgrade to fix the data

--- modules/pagemaster/pntemplates/plugins/function.pmformuploadinput.php
+++ modules/pagemaster/pntemplates/plugins/function.pmformuploadinput.php
@@ -2,11 +2,11 @@
 /**
  * PageMaster
  *
- * @copyright (c) 2008, PageMaster Team
+ * @copyright   (c) PageMaster Team
  * @link        http://code.zikula.org/pagemaster/
  * @license     GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  * @version     $ Id $
- * @package     Zikula_3rd_party_Modules
+ * @package     Zikula_3rdParty_Modules
  * @subpackage  pagemaster
  */
 
@@ -14,73 +14,107 @@ require_once('system/pnForm/plugins/function.pnformuploadinput.php');
 
 class pmformuploadinput extends pnFormUploadInput
 {
-	var $columnDef = 'C(512)';
-	var $title     = _PAGEMASTER_PLUGIN_UPLOAD;
-	var $upl_arr;
+    var $columnDef = 'C(512)';
+    var $title     = _PAGEMASTER_PLUGIN_UPLOAD;
+    var $upl_arr;
 
-	function getFilename()
-	{
-		return __FILE__; // FIXME: may be found in smarty's data???
-	}
+    function getFilename()
+    {
+        return __FILE__; // FIXME: may be found in smarty's data???
+    }
 
-	function postRead($data, $field)
-	{
-		return unserialize($data);
-	}
+    function render(&$render)
+    {
+        $input_html = parent::render($render);
+        return $input_html.' '.$this->upl_arr['orig_name'];
+    }
 
-	function render(&$render)
-	{
-		$input_html = parent::render($render);
-		return $input_html.' '.$this->upl_arr['orig_name'];
-	}
+    function load(&$render, &$params)
+    {
+        $this->loadValue($render, $render->get_template_vars());
+    }
 
-	function load(&$render, &$params)
-	{
-		$this->loadValue($render, $render->get_template_vars());
-	}
+    function loadValue(&$render, &$values)
+    {
+        if (isset($values[$this->dataField]) && !empty($values[$this->dataField])) {
+            $this->upl_arr = unserialize($values[$this->dataField]);
+        }
+    }
 
-	function loadValue(&$render, &$values)
-	{
-		if (array_key_exists($this->dataField, $values))
-		$value = $values[$this->dataField];
-		if ($value !== null)
-		$this->upl_arr = unserialize($value);
-	}
+    function postRead($data, $field)
+    {
+        if (!empty($data)) {
+            $arrTypeData = @unserialize($data);
 
-	function preSave($data, $field)
-	{
-		$id   = $data['id'];
-		$tid  = $data['tid'];
-		$data = $data[$field['name']];
+            if (!is_array($arrTypeData)) {
+                return LogUtil::registerError('pmformuploadinput: '._PAGEMASTER_STOREDDATAINVALID);
+            }
 
-		if ($data['name'] <> '' and !empty ($_FILES)) {
-			$uploadpath = pnModGetVar('pagemaster', 'uploadpath');
-			// TODO: delete the old file
-			$srcTempFilename = $data['tmp_name'];
-			$ext             = strtolower(getExtension($data['name']));
-			$randName        = getNewFileReference();
-			$new_filename    = $randName . '.' . $ext;
-			$dstFilename     = $uploadpath . '/' . $new_filename;
+            $url = pnGetBaseURL().pnModGetVar('pagemaster', 'uploadpath');
+            if (!empty($arrTypeData['tmb_name'])) {
+                $this->upl_arr =  array(
+                         'orig_name' => $arrTypeData['orig_name'],
+                         'file_name' => $url.'/'.$arrTypeData['file_name']
+                );
+            } else {
+                $this->upl_arr = array(
+                         'orig_name' => '',
+                         'file_name' => ''
+                );
+            }
 
-			copy($srcTempFilename, $dstFilename);
+            return $this->upl_arr;
+        }
 
-			$arrTypeData = array (
+        return NULL;
+    }
+
+    function preSave($data, $field)
+    {
+        $id   = $data['id'];
+        $tid  = $data['tid'];
+        $data = $data[$field['name']];
+
+        if ($id != NULL) {
+            // if it's not a new pub get the old upload
+            $old_upload = DBUtil::selectFieldByID('pagemaster_pubdata'.$tid, $field['name'], $id, 'id');
+        }
+
+        
+        if (!empty($data['name'])) {
+            $uploadpath = pnModGetVar('pagemaster', 'uploadpath');
+
+            // delete the old file
+            if ($id != NULL) {
+                $old_upload_arr = unserialize($old_upload);
+                unlink($uploadpath.'/'.$old_upload_arr['file_name']);
+            }
+
+            $srcTempFilename = $data['tmp_name'];
+            $ext             = strtolower(getExtension($data['name']));
+            $randName        = getNewFileReference();
+            $new_filename    = "{$randName}.{$ext}";
+            $dstFilename     = "{$uploadpath}/{$new_filename}";
+
+            copy($srcTempFilename, $dstFilename);
+
+            $arrTypeData = array (
                 'orig_name' => $data['name'],
-                'file_name' => $dstFilename
-			);
-			return serialize($arrTypeData);
+                'file_name' => $new_filename
+            );
 
-		} elseif ($id != NULL) {
-			// if it's not a new pub
-			// return the old image if no new is selected
-			return DBUtil::selectFieldByID('pagemaster_pubdata'.$tid, $field['name'], $id, 'id');
+            return serialize($arrTypeData);
 
-		}
+        } elseif ($id != NULL) {
+            // if it's not a new pub
+            // return the old upload if no new is selected
+            return $old_upload;
+        }
 
-		return NULL;
-	}
+        return NULL;
+    }
 }
 
 function smarty_function_pmformuploadinput($params, &$render) {
-	return $render->pnFormRegisterPlugin('pmformuploadinput', $params);
+    return $render->pnFormRegisterPlugin('pmformuploadinput', $params);
 }
