3e09757d486a5b7328b45c8051be82981c179607
Mateo TibaPalacios <nestormateo@gmail.com>
2010-07-27 10:48:23 +0000

    Removed debug line

--- modules/PageMaster/lib/PageMaster/Generator.php
+++ modules/PageMaster/lib/PageMaster/Generator.php
@@ -258,7 +258,6 @@ class PageMaster_Generator
                 '                {formlabel for=\'core_showinlist\' _'.'_text=\'' . $title_inlist . '\'}'."\n".
                 '                {formcheckbox id=\'core_showinlist\' checked=\'checked\'}'."\n".
                 '            </div>'."\n".
-                '            {formtextinput id=\'core_pid\' textMode="hidden"}'."\n".
                 '        </fieldset>'."\n".
                 "\n".
                 '        {if isset($id)}'."\n".
