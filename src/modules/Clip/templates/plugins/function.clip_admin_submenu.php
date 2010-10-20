b19229224a8cc0e741b44e63940f61dea1973e80
Mateo Tibaquirá Palacios <mateo@zikula.org>
2010-10-20 17:03:43 -0500

    HTML validation fixes. Closes #247

--- src/modules/Clip/templates/plugins/function.clip_admin_submenu.php
+++ src/modules/Clip/templates/plugins/function.clip_admin_submenu.php
@@ -29,53 +29,53 @@ function smarty_function_clip_admin_submenu($params, $view)
 
     // build the output
     $output  = '<div class="z-menu"><span class="z-menuitem-title">';
-    $output .= '<span class="clip-option">'.$pubtype['title'].'</span><span class="clip-option">&raquo;</span>';
+    $output .= '<span class="clip-option">'.DataUtil::formatForDisplay($pubtype['title']).'</span><span class="clip-option">&raquo;</span>';
 
     $func = FormUtil::getPassedValue('func', 'main');
 
     // pubtype form link
     $output .= '<span>';
     if ($func != 'pubtype') {
-        $output .= '<a href="'.ModUtil::url('Clip', 'admin', 'pubtype', array('tid' => $tid)).'">'.__('Options', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubtype', array('tid' => $tid)).'">'.__('Options', $dom).'</a>');
     } else {
-        $output .= '<a>'.__('Options', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a>'.__('Options', $dom).'</a>');
     }
     $output .= '</span> | ';
 
     // edit fields link
     $output .= '<span>';
     if ($func != 'pubfields') {
-        $output .= '<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'">'.__('Fields', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'">'.__('Fields', $dom).'</a>');
     } elseif (isset($params['field']) != '') {
-        $output .= '<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'#newpubfield">'.__('Fields', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'#newpubfield">'.__('Fields', $dom).'</a>');
     } else {
-        $output .= '<a href="#newpubfield">'.__('Fields', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a href="#newpubfield">'.__('Fields', $dom).'</a>');
     }
     $output .= '</span> | ';
 
     // new article link
     $output .= '<span>';
-    $output .= '<a href="'.ModUtil::url('Clip', 'user', 'edit', array('tid' => $tid, 'goto' => 'referer')).'">'.__('New publication', $dom).'</a>';
+    $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'user', 'edit', array('tid' => $tid, 'goto' => 'referer')).'">'.__('New publication', $dom).'</a>');
     $output .= '</span> | ';
 
     // pub list link
     $output .= '<span>';
     if ($func != 'publist') {
-        $output .= '<a href="'.ModUtil::url('Clip', 'admin', 'publist', array('tid' => $tid)).'">'.__('Publication list', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'publist', array('tid' => $tid)).'">'.__('Publication list', $dom).'</a>');
     } else {
-        $output .= '<a>'.__('Publication list', $dom).'</a>';
+        $output .= DataUtil::formatForDisplayHTML('<a>'.__('Publication list', $dom).'</a>');
     }
 
     // show code links
     if ($func == 'showcode') {
         $output .= '<br />';
-        $output .= '<span class="clip-option">'.__('Generate templates', $dom).'</span><span class="clip-option">&raquo;</span>';
-        $output .= '<span>'.($params['mode'] == 'input'      ? '<a>' : '<a href="'.ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'input')).'">') . __('Input template', $dom).'</a></span> | ';
-        $output .= '<span>'.($params['mode'] == 'outputlist' ? '<a>' : '<a href="'.ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'outputlist')).'">') . __('List template', $dom).'</a></span> | ';
-        $output .= '<span>'.($params['mode'] == 'outputfull' ? '<a>' : '<a href="'.ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'outputfull')).'">') . __('Display template', $dom).'</a></span>';
+        $output .= '<span class="clip-option">'.DataUtil::formatForDisplay(__('Generate templates', $dom)).'</span><span class="clip-option">&raquo;</span>';
+        $output .= '<span>'.($params['mode'] == 'input'      ? '<a>' : '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'input'))).'">') . __('Input template', $dom).'</a></span> | ';
+        $output .= '<span>'.($params['mode'] == 'outputlist' ? '<a>' : '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'outputlist'))).'">') . __('List template', $dom).'</a></span> | ';
+        $output .= '<span>'.($params['mode'] == 'outputfull' ? '<a>' : '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'outputfull'))).'">') . __('Display template', $dom).'</a></span>';
     } else {
         $output .= '</span> | ';
-        $output .= '<span><a href="'.ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'input')).'">'.__('Generate templates', $dom).'</a></span>';
+        $output .= '<span>'.DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'showcode', array('tid' => $tid, 'mode' => 'input')).'">'.__('Generate templates', $dom).'</a>').'</span>';
     }
 
     $output .= '</span></div>';
