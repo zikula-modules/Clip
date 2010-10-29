92bac2ef3002ad5c28592f724a434bd881a7332c
Mateo Tibaquirá Palacios <mateo@zikula.org>
2010-10-29 01:50:50 -0500

    [Plugins review] Text core reorganization

--- src/modules/Clip/lib/Clip/Form/Plugin/Text.php
+++ src/modules/Clip/lib/Clip/Form/Plugin/Text.php
@@ -14,13 +14,10 @@ class Clip_Form_Plugin_Text extends Form_Plugin_TextInput
     public $pluginTitle;
     public $columnDef = 'X';
 
-    public $config;
+    public $config = array();
 
     function setup()
     {
-        $dom = ZLanguage::getModuleDomain('Clip');
-        $this->setDomain($dom);
-
         //! field type name
         $this->pluginTitle = $this->__('Text');
     }
@@ -30,18 +27,20 @@ class Clip_Form_Plugin_Text extends Form_Plugin_TextInput
         return __FILE__;
     }
 
-    static function getPluginOutput($field)
+    /**
+     * Form Framework methods.
+     */
+    function readParameters($view, &$params)
     {
-        $body = '{$pubdata.'.$field['name'].'|safehtml|modcallhooks:\'Clip\'}';
+        $this->parseConfig($view->eventHandler->getPubfieldData($params['id'], 'typedata'));
 
-        return array('body' => $body);
+        parent::readParameters($view, $params);
+
+        $this->textMode = 'multiline';
     }
 
     function render($view)
     {
-        $this->parseConfig($view->eventHandler->getPubfieldData($this->id, 'typedata'));
-
-        $this->textMode = 'multiline';
         if ($this->config['usescribite'] && ModUtil::available('scribite')) {
             static $scribite_arr;
             $scribite_arr[] = $this->id;
@@ -49,12 +48,26 @@ class Clip_Form_Plugin_Text extends Form_Plugin_TextInput
                                   array('modulename' => 'Clip',
                                         'editor'     => 'xinha',
                                         'areas'      => $scribite_arr));
+
             PageUtil::addVar('rawtext', $scribite);
         }
 
         return parent::render($view);
     }
 
+    /**
+     * Clip processing methods.
+     */
+    static function getPluginOutput($field)
+    {
+        $body = '{$pubdata.'.$field['name'].'|safehtml|modcallhooks:\'Clip\'}';
+
+        return array('body' => $body);
+    }
+
+    /**
+     * Clip admin methods.
+     */
     static function getSaveTypeDataFunc($field)
     {
         $saveTypeDataFunc = 'function saveTypeData()
@@ -72,13 +85,11 @@ class Clip_Form_Plugin_Text extends Form_Plugin_TextInput
 
     function getTypeHtml($field, $view)
     {
-        $typedata = isset($view->_tpl_vars['typedata']) ? $view->_tpl_vars['typedata'] : false;
-        $this->parseConfig($typedata);
-
-        $checked = $this->config['usescribite'] ? 'checked="checked"' : '';
+        $this->parseConfig($view->_tpl_vars['field']['typedata']);
 
         // TODO Formatting config
         if (ModUtil::available('scribite')) {
+            $checked = $this->config['usescribite'] ? 'checked="checked"' : '';
             $html = '<div class="z-formrow">
                          <label for="clipplugin_usescribite">'.$this->__('Use Scribite!').':</label>
                          <input type="checkbox" id="clipplugin_usescribite" name="clipplugin_usescribite" '.$checked.' />
@@ -97,8 +108,7 @@ class Clip_Form_Plugin_Text extends Form_Plugin_TextInput
      */
     function parseConfig($typedata='')
     {
-        $this->config = array();
-
+        // config string: "(bool)usescribite"
         $this->config['usescribite'] = (bool)$typedata;
     }
 }
