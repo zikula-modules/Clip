3c8dfa1402bfcfdbb953662ed9076da1d2287494
Mateo Tibaquirá Palacios <mateo@zikula.org>
2010-10-29 01:17:52 -0500

    [Plugins review] Integer extension to configure a min/max value

--- src/modules/Clip/lib/Clip/Form/Plugin/Int.php
+++ src/modules/Clip/lib/Clip/Form/Plugin/Int.php
@@ -14,11 +14,10 @@ class Clip_Form_Plugin_Int extends Form_Plugin_IntInput
     public $pluginTitle;
     public $columnDef = 'I4';
 
+    public $config = array();
+
     function setup()
     {
-        $dom = ZLanguage::getModuleDomain('Clip');
-        $this->setDomain($dom);
-
         //! field type name
         $this->pluginTitle = $this->__('Integer Value');
     }
@@ -26,5 +25,67 @@ class Clip_Form_Plugin_Int extends Form_Plugin_IntInput
     function getFilename()
     {
         return __FILE__;
+    }
+    /**
+     * Form Framework methods.
+     */
+    function readParameters($view, &$params)
+    {
+        $this->parseConfig($view->eventHandler->getPubfieldData($params['id'], 'typedata'));
+
+        $params['minValue'] = isset($params['minValue']) ? $params['minValue'] : $this->config['min'];
+        $params['maxValue'] = isset($params['maxValue']) ? $params['maxValue'] : $this->config['max'];
+
+        parent::readParameters($view, $params);
+    }
+
+    /**
+     * Clip admin methods.
+     */
+    static function getSaveTypeDataFunc($field)
+    {
+        $saveTypeDataFunc = 'function saveTypeData()
+                             {
+                                 if ($F(\'clipplugin_minvalue\') != null) {
+                                     $(\'typedata\').value = $F(\'clipplugin_minvalue\');
+                                 }
+                                 $(\'typedata\').value += \'|\';
+                                 if ($F(\'clipplugin_maxvalue\') != null) {
+                                     $(\'typedata\').value += $F(\'clipplugin_maxvalue\');
+                                 }
+                                 closeTypeData();
+                             }';
+
+        return $saveTypeDataFunc;
+    }
+
+    function getTypeHtml($field, $view)
+    {
+        $typedata = isset($view->_tpl_vars['field']['typedata']) ? $view->_tpl_vars['field']['typedata'] : '|';
+        $this->parseConfig($typedata);
+
+        $html = ' <div class="z-formrow">
+                      <label for="clipplugin_minvalue">'.$this->__('Min value').':</label>
+                      <input type="text" id="clipplugin_minvalue" name="clipplugin_minvalue" value="'.$this->config['min'].'" />
+                  </div>
+                  <div class="z-formrow">
+                      <label for="clipplugin_maxvalue">'.$this->__('Max value').':</label>
+                      <input type="text" id="clipplugin_maxvalue" name="clipplugin_maxvalue" value="'.$this->config['max'].'" />
+                  </div>';
+
+        return $html;
+    }
+
+    /**
+     * Parse configuration
+     */
+    function parseConfig($typedata='', $args=array())
+    {
+        $typedata = explode('|', $typedata);
+
+        $this->config = array(
+            'min' => $typedata[0] !== '' ? (float)$typedata[0] : null,
+            'max' => isset($typedata[1]) ? (float)$typedata[1] : null
+        );
     }
 }
