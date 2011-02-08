c2f8ae715e683077394b35c31c4e96de8f41793f
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-02-08 11:09:58 -0500

    Autocompleter config left as plugin parameters: numitems, maxitems, minchars

--- src/modules/Clip/templates/plugins/function.clip_form_relation.php
+++ src/modules/Clip/templates/plugins/function.clip_form_relation.php
@@ -20,6 +20,10 @@ class ClipFormRelation extends Form_Plugin_TextInput
 {
     public $relinfo;
 
+    public $numitems;
+    public $maxitems;
+    public $minchars;
+
     /**
      * Get filename for this plugin.
      *
@@ -45,13 +49,6 @@ class ClipFormRelation extends Form_Plugin_TextInput
         }
 
         if (!is_null($this->relinfo)) {
-            // assign existing data if available
-            if (!($view->_tpl_vars['pubdata'][$this->relinfo['alias']] instanceof Doctrine_Null)) {
-                $this->relinfo['data'] = $view->_tpl_vars['pubdata'][$this->relinfo['alias']];
-            } else {
-                $this->relinfo['data'] = null;
-            }
-
             // detects single or multiple relation
             $this->relinfo['single'] = $this->relinfo['own'] ? ($this->relinfo['type']%2 == 0 ? true : false) : ($this->relinfo['type'] <= 1 ? true : false);
         }
@@ -59,6 +56,24 @@ class ClipFormRelation extends Form_Plugin_TextInput
         $params['textMode'] = 'hidden';
 
         parent::__construct($view, $params);
+    }
+
+    /**
+     * Create event handler.
+     *
+     * @param Form_View $view    Reference to Form_View object.
+     * @param array     &$params Parameters passed from the Smarty plugin function.
+     *
+     * @see    Form_Plugin
+     * @return void
+     */
+    function create($view, &$params)
+    {
+        parent::create($view, $params);
+
+        $this->numitems = (isset($params['numitems']) && is_int($params['numitems'])) ? abs($params['numitems']) : 30;
+        $this->maxitems = (isset($params['maxitems']) && is_int($params['maxitems'])) ? abs($params['maxitems']) : 20;
+        $this->minchars = (isset($params['minchars']) && is_int($params['minchars'])) ? abs($params['minchars']) : 2;
     }
 
     /**
@@ -81,12 +96,6 @@ class ClipFormRelation extends Form_Plugin_TextInput
     {
         $result = parent::render($view);
 
-        // config values
-        // TODO move this to the relation config
-        $numitems = 30;
-        $maxitems = 20;
-        $minchars = 2;
-
         // build the autocompleter setup
         PageUtil::addVar('javascript', 'prototype');
         PageUtil::addVar('javascript', 'modules/Clip/javascript/facebooklist.js');
@@ -100,11 +109,11 @@ class ClipFormRelation extends Form_Plugin_TextInput
                                                     module: "Clip",
                                                     func: "autocomplete",
                                                     tid: '.$this->relinfo['tid'].',
-                                                    itemsperpage: '.$numitems.'
+                                                    itemsperpage: '.$this->numitems.'
                                                   },
-                                                  minchars: '.$minchars.',
-                                                  maxresults: '.$numitems.',
-                                                  maxItems: '.($this->relinfo['single'] ? 1 : $maxitems).'
+                                                  minchars: '.$this->minchars.',
+                                                  maxresults: '.$this->numitems.',
+                                                  maxItems: '.($this->relinfo['single'] ? 1 : $this->maxitems).'
                                                  });
             }
             Event.observe(window, \'load\', clip_enable_'.$this->id.', false);
