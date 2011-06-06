779391f13ef2e2b74c02ee2623d68992f7869035
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-06-06 18:35:17 -0500

    HTML fixes on {clip_adminmenu} links

--- src/modules/Clip/templates/plugins/function.clip_adminmenu.php
+++ src/modules/Clip/templates/plugins/function.clip_adminmenu.php
@@ -48,42 +48,42 @@ function smarty_function_clip_adminmenu($params, Zikula_View &$view)
     // pubtype form link
     $output .= '<span>';
     if ($func != 'pubtype') {
-        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubtype', array('tid' => $tid)).'">'.$view->__('Edit').'</a>');
+        $output .= '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'pubtype', array('tid' => $tid))).'">'.$view->__('Edit').'</a>';
     } else {
-        $output .= DataUtil::formatForDisplayHTML('<a href="#">'.$view->__('Edit').'</a>');
+        $output .= '<a href="#">'.$view->__('Edit').'</a>';
     }
     $output .= '</span> | ';
 
     // edit fields link
     $output .= '<span>';
     if ($func != 'pubfields') {
-        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'">'.$view->__('Fields').'</a>');
+        $output .= '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid))).'">'.$view->__('Fields').'</a>';
     } elseif (isset($params['field']) && $params['field']) {
-        $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid)).'#newpubfield">'.$view->__('Fields').'</a>');
+        $output .= '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'pubfields', array('tid' => $tid))).'#newpubfield">'.$view->__('Fields').'</a>';
     } else {
-        $output .= DataUtil::formatForDisplayHTML('<a href="#newpubfield">'.$view->__('Fields').'</a>');
+        $output .= '<a href="#newpubfield">'.$view->__('Fields').'</a>';
     }
     $output .= '</span> | ';
 
     // relations link
     $output .= '<span>';
-    $output .= DataUtil::formatForDisplayHTML('<a href="'.ModUtil::url('Clip', 'admin', 'relations', array('tid' => $tid, 'withtid1' => $tid, 'op' => 'or', 'withtid2' => $tid)).'">'.$view->__('Relations').'</a>');
+    $output .= '<a href="'.DataUtil::formatForDisplay(ModUtil::url('Clip', 'admin', 'relations', array('tid' => $tid, 'withtid1' => $tid, 'op' => 'or', 'withtid2' => $tid))).'">'.$view->__('Relations').'</a>';
 
     // show code links
     $args = array('func' => 'generator', 'args' => array('tid' => $tid, 'code' => 'form'));
     if ($func == 'generator') {
         $output .= '<br />';
-        $output .= '<span class="clip-option">'.DataUtil::formatForDisplay($view->__('Generate templates')).'</span><span class="clip-option">&raquo;</span>';
+        $output .= '<span class="clip-option">'.$view->__('Generate templates').'</span><span class="clip-option">&raquo;</span>';
         $args['args']['code'] = 'form';
-        $output .= '<span>'.($params['code'] == 'form'      ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">') . $view->__('Input template').'</a></span> | ';
+        $output .= '<span>'.($params['code'] == 'form'      ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">').$view->__('Input template').'</a></span> | ';
         $args['args']['code'] = 'list';
-        $output .= '<span>'.($params['code'] == 'list'      ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">') . $view->__('List template').'</a></span> | ';
+        $output .= '<span>'.($params['code'] == 'list'      ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">').$view->__('List template').'</a></span> | ';
         $args['args']['code'] = 'display';
-        $output .= '<span>'.($params['code'] == 'display'   ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">') . $view->__('Display template').'</a></span> | ';
+        $output .= '<span>'.($params['code'] == 'display'   ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">').$view->__('Display template').'</a></span> | ';
         $args['args']['code'] = 'blocklist';
-        $output .= '<span>'.($params['code'] == 'blocklist' ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">') . $view->__('List block').'</a></span> | ';
+        $output .= '<span>'.($params['code'] == 'blocklist' ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">').$view->__('List block').'</a></span> | ';
         $args['args']['code'] = 'blockpub';
-        $output .= '<span>'.($params['code'] == 'blockpub'  ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">') . $view->__('Pub block').'</a></span>';
+        $output .= '<span>'.($params['code'] == 'blockpub'  ? '<a href="#">' : '<a href="'.smarty_function_clip_url($args, $view).'">').$view->__('Pub block').'</a></span>';
     } else {
         $output .= '</span> | ';
         $output .= '<span><a href="'.smarty_function_clip_url($args, $view).'">'.$view->__('Generate templates').'</a></span>';
