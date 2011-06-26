df82743fa9db9cb82a4748310ce5e45c756ab1dc
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-06-26 15:35:08 -0500

    Post process the object passed to the edit template

--- src/modules/Clip/lib/Clip/Form/Handler/User/Pubedit.php
+++ src/modules/Clip/lib/Clip/Form/Handler/User/Pubedit.php
@@ -87,6 +87,9 @@ class Clip_Form_Handler_User_Pubedit extends Zikula_Form_AbstractHandler
             }
         }
 
+        // post process the pub before include it in the edit template
+        $this->pub->clipPostRead();
+
         // fills the render
         $view->assign('pubdata',   $data)
              ->assign('pubobj',    $this->pub)
