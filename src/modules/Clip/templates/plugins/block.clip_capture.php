a885af062ddf1ea9ed0ab2362a590fd57f92641f
Mateo TibaPalacios <nestormateo@gmail.com>
2012-03-08 12:06:54 -0500

    Corrected clip_capture docblock. Thanks Carsten!

--- src/modules/Clip/templates/plugins/block.clip_capture.php
+++ src/modules/Clip/templates/plugins/block.clip_capture.php
@@ -10,31 +10,21 @@
  */
 
 /**
- * Block to perform a Clip's permission check.
+ * Block to capture a template section into a variable.
  *
  * Available parameters:
- *  - name   (string) Name of the .
  *  - assign (string) Variable name to assign the result to.
  *  - append (string) Variable name to append the result to.
  *
- * Examples:
+ * Example:
  *
- *  For Clip access check:
- *  <samp>{clip_access permlvl=ACCESS_ADMIN}</samp>
- *
- *  For Grouptype access check:
- *  <samp>{clip_access gid=$gid}</samp>
- *
- *  For Pubtype access check:
- *  <samp>{clip_access tid=$pubtype.tid}</samp>
- *
- *  For Publication edit access check:
- *  <samp>{clip_access pub=$pubdata context='edit'}</samp>
+ *  Calpure a post-filtered output:
+ *  <samp>{clip_capture assign='mylist'}{clip_func func='list' tid=$cliptids.blog template='mini'}{/clip_capture}</samp>
  *
  * @param array       $params All parameters passed to this plugin from the template.
  * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
  *
- * @return boolean
+ * @return void
  */
 function smarty_block_clip_capture($params, $content, Zikula_View $view)
 {
