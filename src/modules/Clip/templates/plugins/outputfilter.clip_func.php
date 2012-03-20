e34088aea92833366133ba9897a2f5699faa1f7d
Mateo TibaPalacios <nestormateo@gmail.com>
2012-03-20 02:29:09 -0500

    clip_func outputfilter now handles exceptions like permissions issues.

--- src/modules/Clip/templates/plugins/outputfilter.clip_func.php
+++ src/modules/Clip/templates/plugins/outputfilter.clip_func.php
@@ -25,8 +25,14 @@ function smarty_outputfilter_clip_func($source, $view)
     $num = preg_match_all('/CLIPFUNC:(a:.*?\})/', $source, $matches);
     for ($i = 0; $i < $num; $i++) {
         $params = unserialize($matches[1][$i]);
+        // call the modfunc
+        try {
+            $output = smarty_function_modfunc($params, $view);
+        } catch (Exception $e) {
+            $output = $e->getMessage();
+        }
         // replace them by the modfunc result
-        $source = str_replace($matches[0][$i], smarty_function_modfunc($params, $view), $source);
+        $source = str_replace($matches[0][$i], $output, $source);
     }
  
     // return the modified source
