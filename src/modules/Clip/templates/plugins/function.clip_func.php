a9f7611c8beaf39bc01ec651ab8f179faec785c5
Mateo TibaPalacios <nestormateo@gmail.com>
2012-03-19 20:51:44 -0500

    Plugin facilities added to clip_func too.

--- src/modules/Clip/templates/plugins/function.clip_func.php
+++ src/modules/Clip/templates/plugins/function.clip_func.php
@@ -20,20 +20,18 @@
  *
  * Examples:
  *
- *  <samp>{clip_func func='list' tid=$tid filter="alias:eq:`$rel`"}</samp>
- *  <samp>{clip_func func='display' tid=$tid pid=$pid}</samp>
+ *  <samp>{clip_func func='list' tid=$tid filter="relation:eq:`$id`"}</samp>
+ *  <samp>{clip_func func='display' pub=$pubdata}</samp>
  *
  * @param array       $params All parameters passed to this plugin from the template.
  * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
- *
- * @TODO better to move this to a compiler function?
  *
  * @return mixed The results of the module function.
  */
 function smarty_function_clip_func($params, Zikula_View &$view)
 {
-    if (!isset($params['func']) || !isset($params['tid'])) {
-        $view->trigger_error($view->__f('Error! in %1$s: the %2$s parameter must be specified.', array('clip_func', 'func & tid')));
+    if (!isset($params['func'])) {
+        $view->trigger_error($view->__f('Error! in %1$s: the %2$s parameter must be specified.', array('clip_func', 'func')));
         return false;
     }
 
@@ -42,9 +40,29 @@ function smarty_function_clip_func($params, Zikula_View &$view)
     $params['modname'] = isset($params['modname']) ? $params['modname'] : 'Clip';
     $params['type']    = isset($params['type']) ? $params['type'] : 'user';
 
-    // be sure to define a filter for list calls to not get the input parameters
-    if ($params['func'] == 'list') {
-        $params['filter'] = isset($params['filter']) && !empty($params['filter']) ? $params['filter'] : '()';
+    if ($params['modname'] == 'Clip') {
+        $pubtype = $view->getTplVar('pubtype');
+
+        // setup the tid if not set
+        if (!isset($params['tid'])) {
+            $params['tid'] = $pubtype->tid;
+        }
+
+        // get the publication parameters if passed
+        if (isset($params['pub'])) {
+            $params['tid'] = $params['pub']['core_tid'];
+            if ($params['func'] == 'display' || $params['func'] == 'edit') {
+                $params['pid'] = $params['pub']['core_pid'];
+                $params['id'] = $params['pub']['id'];
+            }
+            unset($params['pub']);
+        }
+
+        // be sure to define a default filter for list calls
+        if ($params['func'] == 'list') {
+            $defaultfilter    = $pubtype->defaultfilter ? $pubtype->defaultfilter : '()';
+            $params['filter'] = isset($params['filter']) && !empty($params['filter']) ? $params['filter'] : $defaultfilter;
+        }
     }
 
     // serialize the parameters to let the output filter to work later
